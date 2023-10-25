0a9b71145ebbe736be16b1f6826a4d630ab771c2
wsalquinga <wilmer_alquinga@outlook.es>
2023-10-24 23:22:35 -0500

    feat: adding validation dependency / add clientDTO

---
+++ src/main/java/com/wsalquinga/clients/dto/ClientDTO.java
@@ -0,0 +1,27 @@
+package com.wsalquinga.clients.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * @author wsalquinga on 24/10/2023
+ */
+@Data
+public class ClientDto {
+
+    private Integer id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastname;
+
+    private String fullname;
+
+    @Email
+    private String email;
+
+    private String address;
+}
