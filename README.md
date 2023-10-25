54bce2a643cbae89cc13059926ccc625dca1f5ed
wsalquinga <wilmer_alquinga@outlook.es>
2023-10-24 22:43:20 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Demo Spring Boot project - Clients Service
+
+## Technologies
+* Spring Boot 3
+* Java 17
+* H2DB (MySQL dialect)
+* Lombok
---
+++ src/test/java/com/wsalquinga/clients/ClientsWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsalquinga.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
