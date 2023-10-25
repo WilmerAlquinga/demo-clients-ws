120a89a025b8bb13dd7699b7998e51279d41d441
wsalquinga <wilmer_alquinga@outlook.es>
2023-10-24 23:03:27 -0500

    feat: adding client model and repository

---
+++ src/main/java/com/wsalquinga/clients/entity/Client.java
@@ -0,0 +1,71 @@
+package com.wsalquinga.clients.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * @author wsalquinga on 24/10/2023
+ */
+
+@Entity
+@Table(name = "client")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "lastname", nullable = false, length = 50)
+    private String lastname;
+
+    @Column(name = "fullname", nullable = false, length = 100)
+    private String fullname;
+
+    @Column(name = "email", nullable = false, length = 60)
+    private String email;
+
+    @Column(name = "address", length = 120)
+    private String address;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    private void setAuditData() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.deletedAt = null;
+        this.fullname = this.name + this.lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return id == client.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/wsalquinga/clients/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.wsalquinga.clients.repository;
+
+import com.wsalquinga.clients.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author wsalquinga on 24/10/2023
+ */
+@Repository
+public interface ClientRepository extends JpaRepository<Integer, Client> {
+}
